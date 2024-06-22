d5a6c09fc2348cf0e61b5fc130de7cfd6adc2121
Ash-2k3 <ash.me.cse@gmail.com>
2024-06-23 02:23:48 +0530

    Draft working 1st version - Images and Docs --pdf/txt files arranger

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from ast import Pass
 import os
 from pathlib import Path
-from collections import UserList
 import shutil
 
 def get_current_working_directory() -> Path:
@@ -11,7 +10,13 @@ def get_file_names():
            return os.listdir(pwd)
 
 def put_in_img_dir(file):
-           Pass
+           pwd = get_current_working_directory()
+           docFilePath = pwd / 'S-Images'
+
+           if not os.path.exists(docFilePath):
+                      os.mkdir('S-Images')
+
+           shutil.move(str(pwd / file), str(docFilePath))
 
 def put_in_doc_dir(file):
            pwd = get_current_working_directory()
